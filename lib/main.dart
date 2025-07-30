81edcb354362fb59f30726b140daecf68465c3fd
Mustafa Kamel <mk8764687@gmail.com>
2025-07-30 20:12:55 +0300

    `Update .gitignore to include lib/core/constants.dart and update main.dart to use VideoCallView instead of VideoCallPage.`

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'features/video_call/presentation/pages/video_call_page.dart';
+import 'features/video_call/presentation/views/video_call_view.dart';
 
 void main() {
   runApp(const MyApp());
@@ -11,7 +11,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
-      home: VideoCallPage(),
+      home: VideoCallView(),
     );
   }
 }
